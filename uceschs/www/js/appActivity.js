82245f4d92de8736b2052c681378f41b96ec2af9
studentuser <uceschs@ucl.ac.uk>
2018-02-02 11:19:45 -0500

    debug -can't find ucl.png

--- uceschs/www/js/appActivity.js
+++ uceschs/www/js/appActivity.js
@@ -2,6 +2,5 @@ function menuClicked() {
   alert("You clicked the menu"); 
 }
 function replaceGraphs() { 
-var image = document.getElementById('graphdiv');
-image.src = "images/ucl.png";
+  document.getElementById("graphdiv").innerHTML ="<img src='images/ucl.png'>";  
 } 
